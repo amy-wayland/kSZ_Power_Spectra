837caee67e3b26f11c975ad515a35fa0fb032a84
Amy Wayland <amy.wayland@physics.ox.ac.uk>
2025-07-01 13:46:16 +0100

    Update kSZ_angular_power_spectra.py
    Built a function to calculate the normalisation of the gas density profile at each scale factor value in the a_arr

--- kSZ_angular_power_spectra.py
+++ kSZ_angular_power_spectra.py
@@ -186,10 +186,10 @@ profile_parameters = {"lMc": 14.0, "beta": 0.6, "eta_b": 0.5, "A_star": 0.03}
 pGas = hp.HaloProfileDensityHE(mass_def=hmd_200m, concentration=cM, kind="rho_gas", **profile_parameters)
 pGas.update_precision_fftlog(padding_lo_fftlog=1e-2, padding_hi_fftlog=1e2, n_per_decade=300, plaw_fourier=-2.0)
 
-# Function to normalise into an overdensity
 def p_gas_normalisation(pgas, a):
     '''
     Calculates the physical gas density at scale factor a for a given gas profile p_gas
+    to normalise the gas density profile
     
     '''
     def rho_gas_integrand(M):
@@ -198,10 +198,37 @@ def p_gas_normalisation(pgas, a):
     
     return hmc.integrate_over_massfunc(rho_gas_integrand, cosmo, a) / a**3
 
-# Normalisation factor
-z_val = 0.55
-a = 1/(1+z_val)
-gas_norm = p_gas_normalisation(pGas, a)
+
+def pk_xe(cosmo, hmc, prof, *,
+                 prof2=None, prof_2pt=None,
+                 p_of_k_a=None,
+                 get_1h=True, get_2h=True,
+                 lk_arr=None, a_arr=None,
+                 extrap_order_lok=1, extrap_order_hik=2,
+                 smooth_transition=None, suppress_1h=None, extrap_pk=False):
+    '''
+    Returns a Pk2D object of the cross-correlation between the electron overdensity
+    with another overdensity profile x correctly normalised
+
+    '''
+    pk_arr = ccl.halos.pk_2pt.halomod_power_spectrum(
+      cosmo, hmc, np.exp(lk_arr), a_arr,
+      prof, prof2=prof2, prof_2pt=prof_2pt, p_of_k_a=p_of_k_a,
+      get_1h=get_1h, get_2h=get_2h,
+      smooth_transition=smooth_transition, suppress_1h=suppress_1h,
+      extrap_pk=extrap_pk)
+    
+    if isinstance(prof2, hp.HaloProfileDensityHE):
+        pk_norm = np.array([p_gas_normalisation(prof, a)**2 for a in a_arr])
+    else:
+        pk_norm = np.array([p_gas_normalisation(prof, a) for a in a_arr])
+    
+    pk_arr_normalised = pk_arr / pk_norm[:, None]
+
+    return ccl.Pk2D(a_arr=a_arr, lk_arr=lk_arr, pk_arr=pk_arr_normalised,
+                extrap_order_lok=extrap_order_lok,
+                extrap_order_hik=extrap_order_hik,
+                is_logp=False)
 
 #%%
 # Cross-correlations
@@ -209,22 +236,22 @@ gas_norm = p_gas_normalisation(pGas, a)
 #%%
 
 # Matter-matter
-pk_mm = ccl.halos.halomod_Pk2D(cosmo, hmc, pM, lk_arr=np.log(k_vals), a_arr=a_arr)
+pk_mm = ccl.halos.halomod_Pk2D(cosmo, hmc, pM, lk_arr=lk_arr, a_arr=a_arr)
 
 # Galaxy-matter
 pk_gm = ccl.halos.halomod_Pk2D(cosmo, hmc, pG, prof2=pM, lk_arr=lk_arr, a_arr=a_arr)
 
 # Electron-matter
-pk_em = (1/gas_norm) * ccl.halos.halomod_Pk2D(cosmo, hmc, pGas, prof2=pM, lk_arr=lk_arr, a_arr=a_arr)
+pk_em = pk_xe(cosmo, hmc, pGas, prof2=pM, lk_arr=lk_arr, a_arr=a_arr)
 
 # Electron-galaxy
-pk_eg = (1/gas_norm) * ccl.halos.halomod_Pk2D(cosmo, hmc, pGas, prof2=pG, lk_arr=lk_arr, a_arr=a_arr)
+pk_eg = pk_xe(cosmo, hmc, pGas, prof2=pG, lk_arr=lk_arr, a_arr=a_arr)
 
 # Electron-galaxy one-halo term only
-pk_eg_1h = (1/gas_norm) * ccl.halos.halomod_Pk2D(cosmo, hmc, pGas, prof2=pG, lk_arr=lk_arr, a_arr=a_arr, get_2h=False)
+pk_eg_1h = pk_xe(cosmo, hmc, pGas, prof2=pG, lk_arr=lk_arr, a_arr=a_arr, get_2h=False)
 
 # Electron-electron
-pk_ee = (1/gas_norm**2) * ccl.halos.halomod_Pk2D(cosmo, hmc, pGas, prof2=pGas, lk_arr=lk_arr, a_arr=a_arr)
+pk_ee = pk_xe(cosmo, hmc, pGas, prof2=pGas, lk_arr=lk_arr, a_arr=a_arr)
 
 # Galaxy-galaxy
 pk_gg = ccl.halos.halomod_Pk2D(cosmo, hmc, pG, prof2=pG, lk_arr=lk_arr, a_arr=a_arr)
@@ -234,6 +261,8 @@ pk_gg = ccl.halos.halomod_Pk2D(cosmo, hmc, pG, prof2=pG, lk_arr=lk_arr, a_arr=a_
 
 #%%
 
+# Perpendicular component
+
 P_of_k_2d_perp_1 = np.zeros((len(k_vals), len(a_arr)))
 P_of_k_2d_perp_2 = np.zeros((len(k_vals), len(a_arr)))
 
@@ -242,6 +271,10 @@ for i, a in enumerate(a_arr):
     P2_perp = np.array([P_perp_2(k, k_prime_vals, pk_em, pk_gm, a, aHf_arr, i) for k in k_vals])
     P_of_k_2d_perp_1[:, i] = P1_perp
     P_of_k_2d_perp_2[:, i] = P2_perp
+    
+#%%
+
+# Parallel component
 
 P_of_k_2d_par_1 = np.zeros((len(k_vals), len(a_arr)))
 P_of_k_2d_par_2 = np.zeros((len(k_vals), len(a_arr)))
@@ -261,11 +294,15 @@ a_arr = a_arr[sorted_indices]
 H_arr = H_arr[sorted_indices]
 f_arr = f_arr[sorted_indices]
 
+#%%
+
 P_of_k_2d_perp_1 = P_of_k_2d_perp_1[:, sorted_indices]
 P_of_k_2d_perp_2 = P_of_k_2d_perp_2[:, sorted_indices]
 
 pk2d_perp_1 = ccl.Pk2D(a_arr=a_arr, lk_arr=np.log(k_vals), pk_arr=P_of_k_2d_perp_1.T, is_logp=False)
 pk2d_perp_2 = ccl.Pk2D(a_arr=a_arr, lk_arr=np.log(k_vals), pk_arr=P_of_k_2d_perp_2.T, is_logp=False)
+
+#%%
 
 P_of_k_2d_par_1 = P_of_k_2d_par_1[:, sorted_indices]
 P_of_k_2d_par_2 = P_of_k_2d_par_2[:, sorted_indices]
@@ -279,14 +316,12 @@ pk2d_par_2 = ccl.Pk2D(a_arr=a_arr, lk_arr=np.log(k_vals), pk_arr=P_of_k_2d_par_2
 #%%
 
 sigma_T_cgs = 6.65e-25 # cm^2
-n_e0_cgs = 2e-7 # cm^{-3}
+n_e0_cgs = 8e-4 # cm^{-3}
 cm_per_Mpc = 3.0857e24 # cm / Mpc
 sigma_T = (sigma_T_cgs / cm_per_Mpc**2) * cosmo['h']**2 # (Mpc/h)**2
 n_e0 = (n_e0_cgs * cm_per_Mpc**3) / cosmo['h']**3 # 1/(Mpc/h)**3
 
 pz = (1 / np.sqrt(2 * np.pi * 0.05**2)) * np.exp(- 0.5 * ((z - 0.55) / 0.05)**2)
-Hz = ccl.h_over_h0(cosmo, 1/(1+z)) * cosmo['h'] * 100
-nz = Hz * pz
 
 sort_idx = np.argsort(z)
 z = z[sort_idx]
@@ -303,11 +338,11 @@ gc_T = ccl.Tracer()
 gc_pi_par = ccl.Tracer()
 gc_T_par = ccl.Tracer()
 
-gc_pi.add_tracer(cosmo, kernel=kernel_pi, der_bessel=0, der_angles=0)
-gc_T.add_tracer(cosmo, kernel=(chi, weight_T), der_bessel=0, der_angles=0)
+gc_pi.add_tracer(cosmo, kernel=kernel_pi)
+gc_T.add_tracer(cosmo, kernel=(chi, weight_T))
 
-gc_pi_par.add_tracer(cosmo, kernel=kernel_pi, der_bessel=1, der_angles=0)
-gc_T_par.add_tracer(cosmo, kernel=(chi, weight_T), der_bessel=1, der_angles=0)
+gc_pi_par.add_tracer(cosmo, kernel=kernel_pi, der_bessel=1)
+gc_T_par.add_tracer(cosmo, kernel=(chi, weight_T), der_bessel=1)
 
 #%%
 # Calculate the angular power spectra
@@ -324,8 +359,8 @@ D_ells_perp_1 = ells * (ells + 1) * C_ells_perp_1 / (2 * np.pi)
 D_ells_perp_2 = ells * (ells + 1) * C_ells_perp_2 / (2 * np.pi)
 D_ells_perp_T = D_ells_perp_1 - D_ells_perp_2
 
-C_ells_par_1 = -sigma_T * n_e0 * ((ells * (ells+1)) / (ells+1/2)**2) * ccl.angular_cl(cosmo, gc_pi_par, gc_T_par, ells, p_of_k_a=pk2d_par_1) / 4
-C_ells_par_2 = -sigma_T * n_e0 * ((ells * (ells+1)) / (ells+1/2)**2) * ccl.angular_cl(cosmo, gc_pi_par, gc_T_par, ells, p_of_k_a=pk2d_par_2) / 4
+C_ells_par_1 = -sigma_T * n_e0 * ccl.angular_cl(cosmo, gc_pi_par, gc_T_par, ells, p_of_k_a=pk2d_par_1) / 4
+C_ells_par_2 = -sigma_T * n_e0 * ccl.angular_cl(cosmo, gc_pi_par, gc_T_par, ells, p_of_k_a=pk2d_par_2) / 4
 C_ells_par_T = C_ells_par_1 + C_ells_par_2
 
 D_ells_par_1 = ells * (ells + 1) * C_ells_par_1 / (2 * np.pi)
@@ -399,6 +434,8 @@ for i, a in enumerate(a_arr):
     P_of_k_2d_par_1_TT[:, i] = P1_par_TT
     P_of_k_2d_par_2_TT[:, i] = P2_par_TT
     
+#%%
+    
 z = (1 / a_arr) - 1
 sorted_indices = np.argsort(a_arr)
 z = z[sorted_indices]
@@ -408,7 +445,7 @@ f_arr = f_arr[sorted_indices]
 
 #%%
 
-# Pk2D objects for pi-pi
+# Pk2D objects for g-g
 
 P_of_k_2d_perp_1_gg = P_of_k_2d_perp_1_gg[:, sorted_indices]
 P_of_k_2d_perp_2_gg = P_of_k_2d_perp_2_gg[:, sorted_indices]
@@ -445,17 +482,18 @@ pk2d_par_2_TT = ccl.Pk2D(a_arr=a_arr, lk_arr=np.log(k_vals), pk_arr=P_of_k_2d_pa
 cmb_data = np.loadtxt('data/camb_93159309_scalcls.dat', usecols=(0,1))
 ell_vals_full = cmb_data[:,0]
 ell_vals_cmb = ell_vals_full[38:7978]
-D_ells_full = cmb_data[:,1]
-D_ells_cmb = D_ells_full[38:7978]
-C_ells_cmb = np.array([2 * np.pi * D_ells_cmb[i] * 1/(ell_vals_cmb[i] * (ell_vals_cmb[i]+1)) for i in range(len(ell_vals_cmb))])
+T_cmb = 2.725e6 # micro Kelvin
+D_ells_full = cmb_data[:,1] / T_cmb**2
+D_ells_cmb = D_ells_full[38:7978] / T_cmb**2
+C_ells_cmb = np.array([2 * np.pi * D_ells_cmb[i] * 1/(ell_vals_cmb[i] * (ell_vals_cmb[i]+1)) for i in range(len(ell_vals_cmb))]) / T_cmb**2
 
+#%%
 
 # Noise power spectrum for C_l^{TT}
 
 d = np.loadtxt("data/SO_LAT_Nell_T_atmv1_baseline_fsky0p4_ILC_CMB.txt", unpack=True)
 l_base = d[0]
 nl_base = d[1]
-
 l_s4 = l_base
 
 # Atmospheric noise
@@ -473,41 +511,45 @@ beam = np.exp(-0.5*l_s4*(l_s4+1)*(np.radians(fwhm/60)/2.355)**2)
 Nwhite = DT**2*(np.pi/180/60)**2
 
 # S4-only noise
-nl_s4 = Nwhite * (1 + (l_s4/lknee)**aknee)/beam**2
+T_cmb = 2.725e6 # micro Kelvin
+nl_s4 = (Nwhite * (1 + (l_s4/lknee)**aknee)/beam**2) / T_cmb**2
 
 # Combine with Planck on large scales
-nl_s4 = 1/(1/nl_s4+1/nl_base)
+nl_s4 = (1/(1/nl_s4+1/nl_base)) / T_cmb**2
 
 #%%
 
-# Calculate the auto-correlation angular power spectra
+# Secondary anisotropies power spectrum for C_l^{T T}
+
+import pickle
+
+act_cells = pickle.load(open("data/P-ACT_theory_cells.pkl", "rb"))
+
+ells_act = act_cells["ell"]
+ells_act = ells_act[38:7978] # Match the ell values to the noise power spectrum
+C_ells_act = act_cells["tt", "dr6_pa5_f090", "dr6_pa5_f090"] / T_cmb**2
+C_ells_act = C_ells_act[38:7978]
+
+#%%
+
+# Shot noise power spectrum for C_l^{g g}
+
+sigma_v = (300e3 / 3e8)
+n_bar = 149 * (np.pi/180)**2 # galaxies per square radian
+nl_pi = sigma_v**2 / n_bar
+
+#%%
 
 ells = np.geomspace(40, 7979, 7940)
-
-C_ells_perp_1_gg = ((ells * (ells+1)) / (ells+1/2)**2) * ccl.angular_cl(cosmo, gc_pi, gc_pi, ells, p_of_k_a=pk2d_perp_1_gg) / 2
-C_ells_perp_2_gg = ((ells * (ells+1)) / (ells+1/2)**2) * ccl.angular_cl(cosmo, gc_pi, gc_pi, ells, p_of_k_a=pk2d_perp_2_gg) / 2
-C_ells_perp_T_gg = C_ells_perp_1_gg - C_ells_perp_2_gg
-
-C_ells_par_1_gg = - ((ells * (ells+1)) / (ells+1/2)**2) * ccl.angular_cl(cosmo, gc_pi_par, gc_pi_par, ells, p_of_k_a=pk2d_par_1_gg) / 4
-C_ells_par_2_gg = - ((ells * (ells+1)) / (ells+1/2)**2) * ccl.angular_cl(cosmo, gc_pi_par, gc_pi_par, ells, p_of_k_a=pk2d_par_2_gg) / 4
-C_ells_par_T_gg = C_ells_par_1_gg - C_ells_par_2_gg
-
-C_ells_perp_1_TT = (n_e0 * sigma_T)**2 * ((ells * (ells+1)) / (ells+1/2)**2) * ccl.angular_cl(cosmo, gc_T, gc_T, ells, p_of_k_a=pk2d_perp_1_TT) / 2
-C_ells_perp_2_TT = (n_e0 * sigma_T)**2 * ((ells * (ells+1)) / (ells+1/2)**2) * ccl.angular_cl(cosmo, gc_T, gc_T, ells, p_of_k_a=pk2d_perp_2_TT) / 2
-C_ells_perp_T_TT = C_ells_perp_1_TT - C_ells_perp_2_TT + C_ells_cmb + nl_s4
-
-C_ells_par_1_TT = - (n_e0 * sigma_T)**2 * ((ells * (ells+1)) / (ells+1/2)**2) * ccl.angular_cl(cosmo, gc_T_par, gc_T_par, ells, p_of_k_a=pk2d_par_1_TT) / 4
-C_ells_par_2_TT = - (n_e0 * sigma_T)**2 * ((ells * (ells+1)) / (ells+1/2)**2) * ccl.angular_cl(cosmo, gc_T_par, gc_T_par, ells, p_of_k_a=pk2d_par_2_TT) / 4
-C_ells_par_T_TT = C_ells_par_1_TT - C_ells_par_2_TT + C_ells_cmb + nl_s4
-
-#%%
-
-# Knox formula
+nl_pi_arr = np.full_like(ells, nl_pi)
+C_ells_TT = C_ells_act + nl_s4
+C_ells_gg = nl_pi_arr
 
 f_sky = 0.4
 d_ell = (7979 - 40) / 7940
 
-cov = (C_ells_perp_T_gg * C_ells_perp_T_TT + C_ells_perp_T**2) / (f_sky * (2 * ells + 1) * d_ell)
+# Knox formula
+cov = np.sqrt((C_ells_gg * C_ells_TT + C_ells_perp_T**2) / (f_sky * (2 * ells + 1) * d_ell))
 
 #%%
 
@@ -517,5 +559,4 @@ plt.loglog()
 plt.xlabel(r'$\ell$', fontsize=20)
 plt.ylabel(r'$C_{\ell}^{\pi T}$', fontsize=20)
 plt.tick_params(which='both', direction='in', width=1, length=3)
-plt.legend(fontsize=12, frameon=False, loc="center right", ncol=2)
 plt.show()
