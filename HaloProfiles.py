f127d31e7c8010cdc32dffef8e355e6c7fc26d82
Amy <amywayland@Amys-MacBook-Pro-2.local>
2025-07-03 10:50:24 +0100

    removed spaces

--- HaloProfiles.py
+++ HaloProfiles.py
@@ -4,8 +4,6 @@ from scipy.integrate import quad, simps
 from scipy.interpolate import RegularGridInterpolator, interp1d
 from scipy.special import sici
 
-
-
 def get_prefac_rho(kind, XH=0.76):
     """Prefactor that transforms gas mas density into
     other types of density, depending on the hydrogen mass
