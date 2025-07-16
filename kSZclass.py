229a1ca3cf645e6af3873f64375170b406eac172
Amy Wayland <amy.wayland@physics.ox.ac.uk>
2025-07-16 10:30:05 +0100

    Update kSZclass.py

--- kSZclass.py
+++ kSZclass.py
@@ -6,7 +6,7 @@ from scipy.special import erf
 
 class kSZclass:
     
-    def __init__(self, cosmo, k_arr, a_arr, pk_dict):
+    def __init__(self, cosmo, k_arr, z_arr, pk_dict):
         '''
         Class to calculate the 3D and angular power spectra for the kSZ effect
         
@@ -14,31 +14,25 @@ class kSZclass:
         ----------
         cosmo: pyccl cosmology object
         k_arr: numpy array of wavenumbers
-        a_arr: numpy array of scale factors
+        z_arr: numpy array of redshifts
         pk_dict: dictionary containing pk2d objects e.g. pk_mm, pk_eg, etc
         
         '''
         self._cosmo = cosmo
         self._k_arr = k_arr
-        self._a_arr = a_arr
+        self._z_arr = z_arr
         self._pk_dict = pk_dict
-        
         self._lk_arr = np.log(self._k_arr)
-        self._z_arr = (1/self._a_arr) - 1
-        self._H_arr = self._cosmo['h'] * ccl.h_over_h0(self._cosmo, self._a_arr) / ccl.physical_constants.CLIGHT_HMPC
-        self._f_arr = self._cosmo.growth_rate(self._a_arr)
-        self._aHf_arr = self._a_arr * self._H_arr * self._f_arr
 
          
-    def P_perp_1(self, k, a, a_index, pk_mm, pk_ab):
+    def _P_perp_1(self, k, a, pk_mm, pk_ab):
         '''
         Calculates the pk_mm and pk_ab convolution for the perpendicular 
         density-weighted peculiar velocity, q = (1+delta)v, mode
         where a = {e,g} and b = {e,g}
         
         '''
-        aHf = self._aHf_arr[a_index]
-        mu_vals = np.linspace(-0.99, 0.99, 128)
+        mu_vals = np.linspace(-0.99, 0.99, 32)
         lk_vals = np.log(np.logspace(-4, 1, 128))
 
         def integrand2(mu, kp):
@@ -54,18 +48,17 @@ class kSZclass:
         integrand = np.array([integrand1(lk) for lk in lk_vals])
         integral = np.trapz(integrand, lk_vals)
         
-        return integral * aHf**2 / (2*np.pi)**2
+        return integral / (2*np.pi)**2
     
 
-    def P_perp_2(self, k, a, a_index, pk_am, pk_bm):
+    def _P_perp_2(self, k, a, pk_am, pk_bm):
         '''
         Calculates the pk_am and pk_bm convolution for the perpendicular
         density-weighted peculiar velocity, q = (1+delta)v, mode
         where a = {e,g} and b = {e,g}
         
         '''
-        aHf = self._aHf_arr[a_index]
-        mu_vals = np.linspace(-0.99, 0.99, 128)
+        mu_vals = np.linspace(-0.99, 0.99, 32)
         lk_vals = np.log(np.logspace(-4, 1, 128))
         
         def integrand2(mu, kp):
@@ -81,18 +74,17 @@ class kSZclass:
         integrand = np.array([integrand1(lk) for lk in lk_vals])
         integral = np.trapz(integrand, lk_vals)
         
-        return integral * aHf**2 / (2*np.pi)**2
+        return integral / (2*np.pi)**2
     
     
-    def P_par_1(self, k, a, a_index, pk_mm, pk_ab):
+    def _P_par_1(self, k, a, pk_mm, pk_ab):
         '''
         Calculates the pk_mm and pk_ab convolution for the parallel
         density-weighted peculiar velocity, q = (1+delta)v, mode
         where a = {e,g} and b = {e,g}
         
         '''
-        aHf = self._aHf_arr[a_index]
-        mu_vals = np.linspace(-0.99, 0.99, 128)
+        mu_vals = np.linspace(-0.99, 0.99, 32)
         lk_vals = np.log(np.logspace(-4, 1, 128))
 
         def integrand2(mu, kp):
@@ -108,18 +100,17 @@ class kSZclass:
         integrand = np.array([integrand1(lk) for lk in lk_vals])
         integral = np.trapz(integrand, lk_vals)
         
-        return integral * aHf**2 / (2*np.pi)**2
+        return integral / (2*np.pi)**2
 
 
-    def P_par_2(self, k, a, a_index, pk_am, pk_bm):
+    def _P_par_2(self, k, a, pk_am, pk_bm):
         '''
         Calculates the pk_am and pk_bm convolution for the parallel
         density-weighted peculiar velocity, q = (1+delta)v, mode
         where a = {e,g} and b = {e,g}
 
         '''
-        aHf = self._aHf_arr[a_index]
-        mu_vals = np.linspace(-0.99, 0.99, 128)
+        mu_vals = np.linspace(-0.99, 0.99, 32)
         lk_vals = np.log(np.logspace(-4, 1, 128))
         
         def integrand2(mu, kp):
@@ -135,40 +126,10 @@ class kSZclass:
         integrand = np.array([integrand1(lk) for lk in lk_vals])
         integral = np.trapz(integrand, lk_vals)
         
-        return integral * aHf**2 / (2*np.pi)**2
+        return integral / (2*np.pi)**2
     
     
-    def get_pk_OV(self):
-        '''
-        Computes the Ostriker-Vishnia (OV) 3D power spectrum
-
-        '''
-        pk_mm = self._pk_dict['mm']['full']
-            
-        pk1 = np.zeros((len(self._k_arr), len(self._a_arr)))
-        pk2 = np.zeros((len(self._k_arr), len(self._a_arr)))
-        
-        for i, a in enumerate(self._a_arr):
-            p1 = np.array([self.P_perp_1(k, a, i, pk_mm, pk_mm) for k in self._k_arr])
-            p2 = np.array([self.P_perp_2(k, a, i, pk_mm, pk_mm) for k in self._k_arr])
-            pk1[:, i] = p1
-            pk2[:, i] = p2
-        
-        sort_idx = np.argsort(self._a_arr)
-        self._a_arr = self._a_arr[sort_idx]
-        self._H_arr = self._H_arr[sort_idx]
-        self._f_arr = self._f_arr[sort_idx]
-        
-        pk1 = pk1[:, sort_idx]
-        pk2 = pk2[:, sort_idx]
-        
-        pk1 = ccl.Pk2D(a_arr=self._a_arr, lk_arr=np.log(self._k_arr), pk_arr=pk1.T, is_logp=False)
-        pk2 = ccl.Pk2D(a_arr=self._a_arr, lk_arr=np.log(self._k_arr), pk_arr=pk2.T, is_logp=False)
-        
-        return pk1 - pk2
-            
-    
-    def _get_pk2d(self, kind, ab, variant='full'):
+    def get_pk2d(self, kind, ab, variant='full'):
         '''
         Computes pk2d objects for the full array of k and a values
         using the integrals P_perp_i or P_par_i where i = {1,2}
@@ -179,7 +140,7 @@ class kSZclass:
               can be either "perp" or "par"
         ab: cross-correlation type
             can be either 'eg', 'gg', or 'ee'
-        variant: can be either 'full', '1h', '2h', or 'sat'
+        variant: can be either 'full', '1h', '2h', or 'cen'
 
         Returns
         -------
@@ -188,12 +149,12 @@ class kSZclass:
 
         '''
         if kind == 'perp':
-            P1 = self.P_perp_1
-            P2 = self.P_perp_2
+            pkf1 = self._P_perp_1
+            pkf2 = self._P_perp_2
             
         elif kind == 'par':
-            P1 = self.P_par_1
-            P2 = self.P_par_2
+            pkf1 = self._P_par_1
+            pkf2 = self._P_par_2
             
         else:
             raise ValueError(f"Unknown power spectrum type {kind}")
@@ -214,32 +175,35 @@ class kSZclass:
         else:
             raise ValueError(f"Unknown cross-correlation type {ab}")
         
-        pk1 = np.zeros((len(self._k_arr), len(self._a_arr)))
-        pk2 = np.zeros((len(self._k_arr), len(self._a_arr)))
-        
         pk_mm = self._pk_dict['mm']['full']
+
+        pk1 = []
+        pk2 = []
+        for i, z in enumerate(self._z_arr):
+            a = 1/(1+z)
+            H = self._cosmo['h'] * ccl.h_over_h0(self._cosmo, a) / ccl.physical_constants.CLIGHT_HMPC
+            f = ccl.growth_rate(self._cosmo, a)
+            aHf = a*H*f
+            pk1.append(np.array([aHf**2 * pkf1(k, a, pk_mm, pk_ab) for k in self._k_arr]))
+            pk2.append(np.array([aHf**2 * pkf2(k, a, pk_am, pk_bm) for k in self._k_arr]))
         
-        for i, a in enumerate(self._a_arr):
-            p1 = np.array([P1(k, a, i, pk_mm, pk_ab) for k in self._k_arr])
-            p2 = np.array([P2(k, a, i, pk_am, pk_bm) for k in self._k_arr])
-            pk1[:, i] = p1
-            pk2[:, i] = p2
+        pk1 = np.array(pk1)
+        pk2 = np.array(pk2)
         
-        sort_idx = np.argsort(self._a_arr)
-        self._a_arr = self._a_arr[sort_idx]
-        self._H_arr = self._H_arr[sort_idx]
-        self._f_arr = self._f_arr[sort_idx]
+        pkf1 = ccl.Pk2D(a_arr=1/(1+self._z_arr[::-1]),
+                       lk_arr=self._lk_arr,
+                       pk_arr=pk1[::-1],
+                       is_logp=False)
         
-        pk1 = pk1[:, sort_idx]
-        pk2 = pk2[:, sort_idx]
+        pkf2 = ccl.Pk2D(a_arr=1/(1+self._z_arr[::-1]),
+                       lk_arr=self._lk_arr,
+                       pk_arr=pk2[::-1],
+                       is_logp=False)
         
-        pk1 = ccl.Pk2D(a_arr=self._a_arr, lk_arr=np.log(self._k_arr), pk_arr=pk1.T, is_logp=False)
-        pk2 = ccl.Pk2D(a_arr=self._a_arr, lk_arr=np.log(self._k_arr), pk_arr=pk2.T, is_logp=False)
-        
-        return pk1, pk2
+        return pkf1, pkf2
     
-    
-    def _get_tracers(self, kind):
+        
+    def get_tracers(self, kind):
         '''
         Computes the tracers for galaxies and the kSZ effect
 
@@ -253,26 +217,27 @@ class kSZclass:
         sigmaT_over_mp = 8.30883107e-17
         ne_times_mp = 0.5 * (1+xH) * self._cosmo['Omega_b'] * self._cosmo['h']**2 * ccl.physical_constants.RHO_CRITICAL
         sigmaTne = ne_times_mp * sigmaT_over_mp
-
-        nz = np.exp(-0.5 * ((self._z_arr - 0.55) / 0.05)**2) 
         
-        sort_idx = np.argsort(self._z_arr)
-        self._z_arr = self._z_arr[sort_idx]
-        nz = nz[sort_idx]
+        zz = np.linspace(0, 2, 1024)
+        nz = np.exp(-(0.5*(zz-0.55)/0.05)**2)
+        
+        #nz_data = np.load("data/dndzs_lrgs_zhou.npz")
+        #zz = nz_data['z']
+        #nz = nz_data['nz2']
 
-        kernel_g = ccl.get_density_kernel(self._cosmo, dndz=(self._z_arr, nz))
-        chis = ccl.comoving_radial_distance(self._cosmo, 1/(1+self._z_arr))
+        kernel_g = ccl.get_density_kernel(self._cosmo, dndz=(zz, nz))
+        chis = ccl.comoving_radial_distance(self._cosmo, 1/(1+zz))
         
         tg = ccl.Tracer()
         tk = ccl.Tracer()
         
         if kind == 'perp': 
             tg.add_tracer(self._cosmo, kernel=kernel_g)
-            tk.add_tracer(self._cosmo, kernel=(chis, sigmaTne/self._a_arr**2))
+            tk.add_tracer(self._cosmo, kernel=(chis, sigmaTne*(1+zz)**2))
             
         elif kind == 'par':
             tg.add_tracer(self._cosmo, kernel=kernel_g, der_bessel=1)
-            tk.add_tracer(self._cosmo, kernel=(chis, sigmaTne/self._a_arr**2), der_bessel=1)
+            tk.add_tracer(self._cosmo, kernel=(chis, sigmaTne*(1+zz)**2), der_bessel=1)
         
         else:
             raise ValueError(f"Unknown power spectrum type {kind}")
@@ -280,6 +245,7 @@ class kSZclass:
         return tg, tk
     
     
+        
     def get_Cl(self, ells, kind, ab, variant='full'):
         '''
         Calculates the angular power spectrum using the 3D power spectrum
@@ -294,7 +260,7 @@ class kSZclass:
               can be either "perp" or "par"
         ab: cross-correlation type
             can be either 'eg', 'gg', or 'ee'
-        variant: can be either 'full', '1h', '2h', or 'sat'
+        variant: can be either 'full', '1h', '2h', or 'cen'
 
         Returns
         -------
@@ -302,8 +268,8 @@ class kSZclass:
         Cl2: angular power spectrum from the pk_am and pk_bm convolution
 
         '''        
-        tg, tk = self._get_tracers(kind)
-        pk1, pk2 = self._get_pk2d(kind, ab, variant)
+        tg, tk = self.get_tracers(kind)
+        pk1, pk2 = self.get_pk2d(kind, ab, variant)
         
         if kind == 'perp':
             prefac = 0.5 * ells * (ells+1) / (ells+0.5)**2
@@ -338,18 +304,6 @@ class kSZclass:
 
         '''
         return ells * (ells + 1) * Cl / (2 * np.pi)
-    
-    
-    def get_Cl_OV(self, ells):
-        '''
-        Computes the Ostriker-Vishnia (OV) angular power spectrum
-
-        '''
-        tg, tk = self._get_tracer(kind="perp")
-        pk = self.get_pk_OV()
-        prefac = 0.5 * ells * (ells+1) / (ells+0.5)**2
-        Cl = prefac * ccl.angular_cl(self._cosmo, tg, tk, ells, p_of_k_a=pk)
-        return Cl
     
     
 #%%
